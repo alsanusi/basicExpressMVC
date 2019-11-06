7163b9e242d0c3639161597bb11883c83cd04bfb
Als <malkautsars@gmail.com>
2019-11-06 12:53:00 +0800

    Init View

--- app.js
+++ app.js
@@ -1,10 +1,82 @@
+//Load app server using Express
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Haha')
-})
+//Load MySQL Configuration
+const mysql = require('mysql')
+const myConnection = require('express-myconnection')
 
-app.listen(3003, () => {
-    console.log('Server running at port 3003: http://127.0.0.1:3003')
+//Load DB Configuration
+const dbConfig = require('./config')
+const dbOptions = {
+    host: dbConfig.database.host,
+    user: dbConfig.database.user,
+    password: dbConfig.database.password,
+    port: dbConfig.database.port,
+    database: dbConfig.database.database
+}
+
+/**
+ * 3 strategies can be used
+ * single: Creates single database connection which is never closed.
+ * pool: Creates pool of connections. Connection is auto release when response ends.
+ * request: Creates new connection per new request. Connection is auto close when response ends.
+ */
+app.use(myConnection(mysql, dbOptions, 'pool'))
+
+//Setting up templaing view engine - EJS
+app.set('view engine', 'ejs')
+app.use(express.static("views"))
+
+// Express Validator Middleware for Form Validation
+const expressValidator = require('express-validator')
+app.use(expressValidator())
+
+// body-parser is used to read HTTP POST data from Form Input.
+var bodyParser = require('body-parser')
+// bodyParser.urlencoded() parses the text as URL encoded data.
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+// Flash messages in order to show success or error message.
+const flash = require('express-flash')
+app.use(flash())
+
+// Flash require Session
+// Express-Session
+const session = require('express-session');
+app.use(session({
+    cookie: {
+        maxAge: 6000
+    },
+    secret: 'weuw',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// Method-Override
+var methodOverride = require('method-override')
+// Custom logic for overriding method
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
+// Show the Employee Route
+const employeeRoute = require('./routes/employee')
+app.get('/', employeeRoute.showData)
+app.get('/input', employeeRoute.showInputData)
+app.post('/input', employeeRoute.inputData)
+app.get('/edit/(:id)', employeeRoute.showEditData)
+app.put('/edit/(:id)', employeeRoute.editData)
+app.delete('/delete/(:id)', employeeRoute.deleteData)
+
+//Localhost:3003
+app.listen(3000, () => {
+    console.log('Server running at port 3000: http://127.0.0.1:3000')
 })
---
+++ routes/employee.js
@@ -0,0 +1,175 @@
+
+// Routing Process
+// Show the Default Web Page and View all the EmployeeData
+exports.showData = (req, res) => {
+    req.getConnection(function (err, con) {
+        con.query('SELECT * FROM employeeData ORDER BY id DESC', function (err, rows, fields) {
+            if (err) {
+                req.flash('error', err)
+                res.render('index', {
+                    data: ''
+                })
+            } else {
+                res.render('index', {
+                    data: rows
+                })
+            }
+        })
+    })
+}
+
+// Show Employee Data
+exports.showInputData = (req, res) => {
+    res.render('input', {
+        name: '',
+        email: '',
+        address: '',
+        phone: ''
+    })
+}
+
+// Input Employee Data
+exports.inputData = (req, res) => {
+    // Input Form Validation
+    req.assert('name', 'Required Name!').notEmpty()
+    req.assert('email', 'Require Email!').notEmpty()
+    req.assert('address', 'Require Address!').notEmpty()
+    req.assert('phone', 'Required Phone Number!').notEmpty()
+
+    var errors = req.validationErrors()
+
+    if (!errors) {
+        var employeeData = {
+            name: req.sanitize('name').escape().trim(),
+            email: req.sanitize('email').escape().trim(),
+            address: req.sanitize('address').escape().trim(),
+            phone: req.sanitize('phone').escape().trim()
+        }
+        req.getConnection(function (err, con) {
+            con.query('INSERT INTO employeeData SET ?', employeeData, function (err, result) {
+                // If Throw Error
+                if (err) {
+                    req.flash('error', err)
+                    res.render('input', {
+                        name: employeeData.name,
+                        email: employeeData.email,
+                        address: employeeData.address,
+                        phone: employeeData.phone
+                    })
+                    // res.redirect('/')
+                } else {
+                    req.flash('success', 'Employee Data Input Successfully!')
+                    res.render('input', {
+                        name: '',
+                        email: '',
+                        address: '',
+                        phone: ''
+                    })
+                    // res.redirect('/')
+                }
+            })
+        })
+    } else {
+        // When error occurs, the message will show.
+        var error_msg = ''
+        errors.forEach(function (error) {
+            error_msg += error.msg + '</br>'
+        })
+        req.flash('error', error_msg)
+        res.render('input', {
+            name: req.body.name,
+            email: req.body.email,
+            address: req.body.address,
+            phone: req.body.phone
+        })
+    }
+}
+
+// Show Employee Data in Edit Section
+exports.showEditData = (req, res) => {
+    req.getConnection(function (err, con) {
+        con.query('SELECT * FROM employeeData WHERE id = ?', [req.params.id], function (err, rows, fields) {
+            if (err) {
+                throw err
+            } else {
+                res.render('edit', {
+                    id: rows[0].id,
+                    name: rows[0].name,
+                    email: rows[0].email,
+                    address: rows[0].address,
+                    phone: rows[0].phone
+                })
+            }
+        })
+    })
+}
+
+// Update Employee Data
+exports.editData = (req, res) => {
+    // Input Form Validation
+    req.assert('name', 'Required Name!').notEmpty()
+    req.assert('email', 'Require Email!').notEmpty()
+    req.assert('address', 'Require Address!').notEmpty()
+    req.assert('phone', 'Required Phone Number!').notEmpty()
+
+    var errors = req.validationErrors()
+
+    if (!errors) {
+        var employeeData = {
+            name: req.sanitize('name').escape().trim(),
+            email: req.sanitize('email').escape().trim(),
+            address: req.sanitize('address').escape().trim(),
+            phone: req.sanitize('phone').escape().trim()
+        }
+        req.getConnection(function (err, con) {
+            con.query('UPDATE employeeData SET ? WHERE id =' + req.params.id, employeeData, function (err, result) {
+                // If Throw Error
+                if (err) {
+                    req.flash('error', err)
+                    res.render('edit', {
+                        id: req.params.id,
+                        name: req.body.name,
+                        email: req.body.email,
+                        address: req.body.address,
+                        phone: req.body.phone
+                    })
+                    // res.redirect('/')
+                } else {
+                    req.flash('success', 'Employee Data Input Successfully!')
+                    res.redirect('/')
+                }
+            })
+        })
+    } else {
+        // When error occurs, the message will show.
+        var error_msg = ''
+        errors.forEach(function (error) {
+            error_msg += error.msg + '</br>'
+        })
+        req.flash('error', error_msg)
+        res.render('edit', {
+            id: req.params.id,
+            name: req.body.name,
+            email: req.body.email,
+            address: req.body.address,
+            phone: req.body.phone
+        })
+    }
+}
+
+// Delete Employee Data
+exports.deleteData = (req, res) => {
+    var employeeId = {
+        id: req.params.id
+    }
+
+    req.getConnection(function (err, con) {
+        con.query('DELETE FROM employeeData WHERE id = ' + req.params.id, employeeId, function (err, result) {
+            if (err) {
+                throw err
+            } else {
+                res.redirect('/')
+            }
+        })
+    })
+}
