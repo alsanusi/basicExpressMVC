a6583a10ad79bd302edfbccfb17dc31ab5f0429c
Als <malkautsars@gmail.com>
2019-11-06 12:31:20 +0800

    Package Install

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Haha')
+})
+
 app.listen(3003, () => {
     console.log('Server running at port 3003: http://127.0.0.1:3003')
 })
